412e69b2e625c6ae8f1c14701976cf620a5beb3d
Oliver van den Bosch <vanders06@icloud.com>
2021-04-15 16:29:03 +0100

    Create do while

---
+++ loops/dowhile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// Do while loop, does the loop and then checks the condition
+
+int main() {
+
+    int index = 5;
+    do {
+        cout << index << endl;
+        index++;
+    } while (index <=5);
+
+    return 0;
+}
