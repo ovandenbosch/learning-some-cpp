6e005118fc80e98db283dcadd11349f711598e15
Oliver van den Bosch <vanders06@icloud.com>
2021-04-15 14:20:22 +0100

    Make main function

--- daysoftheweek.cpp
+++ daysoftheweek.cpp
@@ -5,11 +5,47 @@ using namespace std;
 string getDayOfWeek(int dayNum) {
     string dayName;
 
+    switch (dayNum)
+    {
+    case 0:
+        dayName = "Monday";
+        break;
     
+    case 1:
+        dayName = "Tuesday";
+        break;
+    
+    case 2:
+        dayName = "Wednesday";
+        break;
+
+    case 3:
+        dayName = "Thursday";
+        break;
+    
+    case 4:
+        dayName = "Friday";
+        break;
+
+    case 5:
+        dayName = "Saturday";
+        break;
+
+    case 6:
+        dayName = "Sunday";
+        break;
+
+    default:
+        dayName = "Invalid Day Number";
+    
+    }
 
     return dayName;
 }
 
 int main() {
+
+    cout << getDayOfWeek(3);
+
 
 }
