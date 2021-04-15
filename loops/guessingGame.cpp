69e3508507f594166d9888b68adfb55d5a6705b4
Oliver van den Bosch <vanders06@icloud.com>
2021-04-15 17:58:01 +0100

    Add rate limiter

--- loops/guessingGame.cpp
+++ loops/guessingGame.cpp
@@ -5,13 +5,26 @@ int main() {
 
     int secretNum = 7;
     int guess;
+    int guessCount = 0;
+    int guessLimit = 3;
+    bool outOfGuesses = false;
 
-    while(secretNum != guess) {
-        cout << "Enter guess: ";
-        cin >> guess;
+
+    while(secretNum != guess && !outOfGuesses) {
+        if (guessCount < guessLimit) {
+            cout << "Enter guess: ";
+            cin >> guess;
+            guessCount ++;
+        } else {
+            outOfGuesses = true;
+        }
     }
 
+    if (outOfGuesses) {
+        cout << "You lose, because you are out of guesses!";
+    } else {
     cout << "You guessed the right number! Well done.";
+    }
 
     return 0;
 }
