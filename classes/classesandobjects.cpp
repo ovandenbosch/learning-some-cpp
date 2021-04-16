9830c774b8e7365eae2e3119a9ab8cad025245e7
Oliver van den Bosch <vanders06@icloud.com>
2021-04-16 14:05:30 +0100

    Use constructors

--- classes/classesandobjects.cpp
+++ classes/classesandobjects.cpp
@@ -6,26 +6,34 @@ class Book {
         string title;
         string author;
         int pages;
+
+        // Constructor
+        Book(string aTitle, string aAuthor, int aPages) {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
 };
 
 int main() {
 
-    Book book1;
-    book1.title = "To Kill a Mockingbird";
-    book1.author = "Harper Lee";
-    book1.pages = 281;
+    Book book1("To kill a mockingbird", "Harper Lee", 281);
+    Book book2("Book2", "Author 2", 396);
+    Book book3("Book 3", "Author 3", 304);
 
-    Book book2;
+    /* Without constructors (very long)
+    Book book2("Book 2");
     book2.title = "Book2";
     book2.author = "Author 2";
     book2.pages = 697;
 
-    Book book3;
+    Book book3("Book 3");
     book3.title = "Book 3";
     book3.author = "Author 3";
     book3.pages = 349;
+    */
 
-    cout << book3.pages;
+    cout << book1.title;
 
     return 0;
 }
